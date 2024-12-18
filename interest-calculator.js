20f58b72f94999c6950e32c95fbd569e579ff03d
r3trolicia <xeslashxem69420@gmail.com>
2024-12-17 21:10:13 -0800

    Update interest-calculator.js

--- interest-calculator.js
+++ interest-calculator.js
@@ -4,6 +4,6 @@ function calculateSimpleInterest(principal, rate, time) {
 
 let principal = readInt("Enter principal (in dollars): ); 
 let rate = readInt("Enter annual interest rate in percent: ");
-let time = readInt("Enter time in years: ")
+let time = readInt("Enter time in years: ");
 
 console.log("Simple Interest is: $", calculateSimpleInterest(principal, rate, time));
