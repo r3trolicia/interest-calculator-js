267e7254007d686718d7192bd83d0c45614fa012
r3trolicia <xeslashxem69420@gmail.com>
2024-12-17 21:19:40 -0800

    Update interest-calculator.js
    browser adaptibility

--- interest-calculator.js
+++ interest-calculator.js
@@ -2,8 +2,8 @@ function calculateSimpleInterest(principal, rate, time) {
     return (principal * rate * time) / 100;
 }
 
-let principal = readInt("Enter principal in dollars: "); 
-let rate = readInt("Enter annual interest rate in percent: ");
-let time = readInt("Enter time in years: ");
+let principal = parseInt(prompt("Enter principal in dollars: ")); 
+let rate = parseInt(prompt("Enter annual interest rate in percent: "));
+let time = parseInt(prompt("Enter time in years: "));
 
 console.log("Simple Interest is: $", calculateSimpleInterest(principal, rate, time));
