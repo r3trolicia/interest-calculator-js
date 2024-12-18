364586b715065809b84c430368ea97fe42e55d76
r3trolicia <xeslashxem69420@gmail.com>
2024-12-17 21:15:31 -0800

    Update interest-calculator.js
    typo

--- interest-calculator.js
+++ interest-calculator.js
@@ -2,7 +2,7 @@ function calculateSimpleInterest(principal, rate, time) {
     return (principal * rate * time) / 100;
 }
 
-let principal = readInt("Enter principal (in dollars): ); 
+let principal = readInt("Enter principal in dollars: "); 
 let rate = readInt("Enter annual interest rate in percent: ");
 let time = readInt("Enter time in years: ");
 
